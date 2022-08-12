0d4c5423a10d769bc98348cf8796601cadfc8f77
Gabriel Borges <gbdesigns13@gmail.com>
2022-08-12 18:06:41 -0300

    (Desafio 3): Apenas posts com label Published

--- github-blog/src/pages/Blog/index.tsx
+++ github-blog/src/pages/Blog/index.tsx
@@ -30,7 +30,7 @@ export function Blog() {
       try {
         setIsLoading(true);
         const response = await api.get(
-          `/search/issues?q=${query}%20repo:${username}/${repoName}`
+          `/search/issues?q=${query}%20label:published%20repo:${username}/${repoName}`
         );
 
         setPosts(response.data.items);
